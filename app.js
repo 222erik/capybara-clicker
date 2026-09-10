a955c5ab38ab85dbb48969135ad3dbad062d38ea
Erik <erikolsson0@proton.me>
2026-09-10 19:06:53 +0200

    cps and background

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 capybara = document.getElementById("capybara")
 clicks = document.getElementById("capy-clicks-indicator")
+cps = document.getElementById("cps-indicator")
+
+var cps_counter = 0
 
 capybara.addEventListener("click", () => {
+    cps_counter++
     clicks.innerText = parseInt(clicks.innerText) + 1
     capybara.classList.add("shrink")
     setTimeout(() => capybara.classList.remove("shrink"), 100)
 })
+
+setInterval(() => {
+    cps.innerText = cps_counter + " cps"
+    cps_counter = 0
+}, 1000)
