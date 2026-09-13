b2286229c3b42ba50c76ab6887dbf87182b7d079
Erik <erikolsson0@proton.me>
2026-09-13 19:29:25 +0200

    work on shop

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ setInterval(() => {
     cps.innerText = cps_counter + " cps"
     cps_counter = 0
 }, 1000)
+
+shop_items = document.getElementsByClassName("shop-item");
+var counter = 0
+for (const item of shop_items) {
+    item.addEventListener("click", () => {
+        switch (counter) {
+            case 0:
+            // +1 Unit per click
+        }
+    })
+    counter++
+}
