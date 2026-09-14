dd901017f3eb515fa7ba04254e361fe27f5707b4
Erik <erikolsson0@proton.me>
2026-09-14 20:54:01 +0200

    add item to shop

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ clicks = document.getElementById("capy-clicks-indicator")
 cps = document.getElementById("cps-indicator")
 
 var cps_counter = 0
+var units_per_click = 1
 
 capybara.addEventListener("click", () => {
-    cps_counter++
-    clicks.innerText = parseInt(clicks.innerText) + 1
+    cps_counter++;
+    clicks.innerText = parseInt(clicks.innerText) + units_per_click;
     capybara.classList.add("shrink")
     setTimeout(() => capybara.classList.remove("shrink"), 100)
+    updateShop()
 })
 
 setInterval(() => {
@@ -19,11 +21,31 @@ setInterval(() => {
 shop_items = document.getElementsByClassName("shop-item");
 var counter = 0
 for (const item of shop_items) {
-    item.addEventListener("click", () => {
-        switch (counter) {
-            case 0:
-            // +1 Unit per click
-        }
-    })
+    var shop_function = () => { }
+    switch (counter) {
+        case 0:
+            shop_function = () => {
+                if (parseInt(clicks.innerText) >= 10) {
+                    clicks.innerText = parseInt(clicks.innerText) - 10
+                    units_per_click++;
+                } else {
+                    // Cannot buy
+                }
+                updateShop()
+            }
+    }
+    item.addEventListener("click", shop_function)
     counter++
 }
+
+function updateShop() {
+    let items = document.getElementsByClassName("shop-item")
+    for (const item of items) {
+        if (parseInt(item.querySelector(".shop-price").innerText) <= parseInt(clicks.innerText)) {
+            item.style.background = "linear-gradient(135deg, #B9AED2, #ABA0C4)";
+        } else {
+            item.style.background = "#999999";
+        }
+    }
+}
+updateShop()
