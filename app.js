6989c73f3c78a7c333b9bf653d47059e90742e88
Erik <erikolsson0@proton.me>
2026-09-15 19:50:32 +0200

    saved in local storage

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 capybara = document.getElementById("capybara")
-clicks = document.getElementById("capy-clicks-indicator")
-cps = document.getElementById("cps-indicator")
-click_power = document.getElementById("click-power-indicator")
+clicks_element = document.getElementById("capy-clicks-indicator")
+cps_element = document.getElementById("cps-indicator")
+click_power_element = document.getElementById("click-power-indicator")
 
 var cps_counter = 0
-var units_per_click = 1
+var units_per_click = parseInt(localStorage.getItem("units_per_click")) || 1
+var clicks = parseInt(localStorage.getItem("clicks")) || 0
+
+click_power_element.innerText = "+" + units_per_click + "/click"
+
+function updateLocalStorage() {
+    localStorage.setItem("units_per_click", units_per_click)
+    localStorage.setItem("clicks", clicks)
+}
+
+function updateDOMItems() {
+    clicks_element.innerText = clicks + units_per_click;
+    click_power_element.innerText = "+" + units_per_click + "/click"
+}
+updateDOMItems()
 
 capybara.addEventListener("click", () => {
     cps_counter++;
-    clicks.innerText = parseInt(clicks.innerText) + units_per_click;
+    clicks += units_per_click;
     capybara.classList.add("shrink")
     setTimeout(() => capybara.classList.remove("shrink"), 100)
     updateShop()
+    updateLocalStorage()
+    updateDOMItems()
 })
 
 setInterval(() => {
-    cps.innerText = cps_counter + " cps"
+    cps_element.innerText = cps_counter + " cps"
     cps_counter = 0
 }, 1000)
 
@@ -26,18 +42,19 @@ for (const item of shop_items) {
     switch (counter) {
         case 0:
             shop_function = () => {
-                if (parseInt(clicks.innerText) >= 10) {
-                    clicks.innerText = parseInt(clicks.innerText) - 10
+                if (clicks >= 10) {
+                    clicks -= 10
                     units_per_click++;
-                    click_power.innerText = "+" + units_per_click + "/click"
                     item.classList.add("flash")
                     setTimeout(() => {
                         item.classList.remove("flash");
                     }, 100)
+                    updateShop()
+                    updateLocalStorage()
+                    updateDOMItems()
                 } else {
                     // Cannot buy
                 }
-                updateShop()
             }
     }
     item.addEventListener("click", shop_function)
@@ -47,7 +64,8 @@ for (const item of shop_items) {
 function updateShop() {
     let items = document.getElementsByClassName("shop-item")
     for (const item of items) {
-        if (parseInt(item.querySelector(".shop-price").innerText) <= parseInt(clicks.innerText)) {
+        if (item.innerHTML === "item") { continue; } // Test item
+        if (parseInt(item.querySelector(".shop-price").innerText) <= clicks) {
             item.style.background = "linear-gradient(135deg, #B9AED2, #ABA0C4)";
         } else {
             item.style.background = "#999999";
